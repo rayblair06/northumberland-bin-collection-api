d07c54b46b74bdaaa98fab3e2a3e5f0efc1ece0c
Ray Blair <rayblair06@hotmail.com>
2022-10-01 15:14:43 +0100

    Delete scrap file if it is older than 7 days

--- src/http/api/controllers/base.ts
+++ src/http/api/controllers/base.ts
@@ -3,7 +3,7 @@ import * as fs from 'fs';
 import { Request, Response, NextFunction } from 'express';
 import { StatusCodes } from 'http-status-codes';
 
-import { checkFoldersExist } from '../../../services/filesystem';
+import { checkFoldersExist, checkScrapeFile } from '../../../services/filesystem';
 import { process } from '../../../services/process';
 import { scrape } from '../../../services/scrape';
 
@@ -12,19 +12,21 @@ export const index = async (request: Request, response: Response, next: NextFunc
     try {
         const house = request.query.house;
         const postcode = request.query.postcode;
+        const filepath = `results/${postcode}.html`;
 
         checkFoldersExist();
+        checkScrapeFile(filepath);
 
         let data;
 
         try {
-            data = fs.readFileSync(`results/${postcode}.html`, { encoding: 'utf-8' });
+            data = fs.readFileSync(filepath, { encoding: 'utf-8' });
         } catch (error) {
             console.log(error);
 
             data = await scrape(house, postcode);
 
-            fs.writeFileSync(`results/${postcode}.html`, data);
+            fs.writeFileSync(filepath, data);
         }
 
         const result = await process(data);
--- src/services/filesystem.ts
+++ src/services/filesystem.ts
@@ -10,3 +10,28 @@ export const checkFoldersExist = () => {
         fs.mkdir('results', () => {});
     }
 };
+
+/**
+ * Check Scrape File
+ * 
+ * @param filepath 
+ */
+export const checkScrapeFile = (filepath) => {
+    fs.stat(filepath, (error, stats) => {
+        const SEVEN_DAYS_AGO = 7 * 24 * 60 * 60 * 1000;
+
+        if (error) {
+            return;
+        }
+
+        // Check if file is older than 7 days
+        if ((new Date().getTime() - new Date(stats.birthtime).getTime()) > SEVEN_DAYS_AGO) {
+            // Delete file
+            fs.unlink(filepath, (err) => {
+                if (err)  {
+                    return console.log(err);
+                }
+            });
+        }
+    });
+};
